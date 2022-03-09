583bba9df8e785dddd6741d5343c6c91c7e07a43
shafaqqat <shqazi1999@gmail.com>
2022-03-09 11:26:33 +0530

    object in js

---
+++ module2/object.js
@@ -0,0 +1,25 @@
+// empty object
+
+let obj = {};
+console.log(obj);
+
+let objAman ={
+    Name : "moh aman",Age:23 ,PhoneNo:7222953333 , LastName:"aman"
+};
+console.log(objAman);
+
+let capAmerica = {
+    Name:'Steve' ,
+    Age: 9999,
+    Frinds : ['tony','hulk','Bruce'],
+    Address: {
+        city: 'Queen',
+        State: 'Haryana'
+    },
+
+    sayHi:function(){
+        console.log("cap america say hiii");
+    }
+}
+
+console.log(capAmerica);
